b3dde74f997fb4262d93b91f413ca294219327c3
Richard Drake <rdrake98@gmail.com>
2014-07-08 17:48:21 +0100

    CA

--- test.rb
+++ test.rb
@@ -1,7 +1,4 @@
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
-driver.get "http://google.com"
-element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
-element.submit
+driver.get "http://climateaudit.org"
 puts "Page title is #{driver.title}"
