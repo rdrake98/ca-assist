982f07b09c0a998583c68aa02f7a71c443953f92
Richard Drake <rdrake98@gmail.com>
2014-07-08 18:20:41 +0100

    use MiniTest

--- test.rb
+++ test.rb
@@ -1,4 +1,16 @@
 require 'selenium-webdriver'
-driver = Selenium::WebDriver.for :firefox
-driver.get "http://climateaudit.org"
-puts "Page title is #{driver.title}"
+require 'minitest/autorun'
+
+class TestClimfit < MiniTest::Test
+  
+  describe "basics" do
+    before do
+      @driver = Selenium::WebDriver.for :firefox
+    end
+    
+    it "should have correct title" do
+      @driver.get "http://climateaudit.org"
+      assert_equal "Climate Audit", @driver.title
+    end
+  end
+end
