077268296bdac8fc2ed0896b6b1cc997a3fa8a2b
Richard Drake <rdrake98@gmail.com>
2014-07-09 17:44:30 +0100

    refactor

--- test.rb
+++ test.rb
@@ -7,20 +7,23 @@ class TestClimfit < MiniTest::Test
   @@driver = Selenium::WebDriver.for :firefox, profile: "caa"
 
   Minitest.after_run { @@driver.quit }
+  
+  def self.get_noko(url)
+    @@driver.get url
+    Nokogiri::HTML @@driver.page_source
+  end
 
   describe "basics" do
     
     it "should have correct CA title" do
-      @@driver.get "http://climateaudit.org"
-      doc = Nokogiri::HTML(@@driver.page_source)
+      doc = TestClimfit.get_noko "http://climateaudit.org"
       assert_equal "Climate Audit", doc.title.strip
     end
     
     it "should have correct WUWT title" do
-      @@driver.get(
+      doc = TestClimfit.get_noko(
         "http://wattsupwiththat.com/2014/07/08/record-levels-of-solar-ultraviolet-measured-in-south-america/"
       )
-      doc = Nokogiri::HTML(@@driver.page_source)
       assert_equal(
         "Record levels of solar ultraviolet measured in South America | Watts Up With That?", 
         doc.title.strip
