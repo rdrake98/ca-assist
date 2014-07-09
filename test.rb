26d723d2e2710ce18622be10498d6a38ababdf11
Richard Drake <rdrake98@gmail.com>
2014-07-09 17:02:40 +0100

    one session with quit at end

--- test.rb
+++ test.rb
@@ -4,26 +4,23 @@ require 'nokogiri'
 
 class TestClimfit < MiniTest::Test
   
+  @@driver = Selenium::WebDriver.for :firefox, profile: "caa"
+
+  Minitest.after_run { @@driver.quit }
+
   describe "basics" do
-    before do
-      @driver = Selenium::WebDriver.for :firefox, profile: "caa"
-    end
-    
-    after do
-      # @driver.quit
-    end
     
     it "should have correct CA title" do
-      @driver.get "http://climateaudit.org"
-      doc = Nokogiri::HTML(@driver.page_source)
+      @@driver.get "http://climateaudit.org"
+      doc = Nokogiri::HTML(@@driver.page_source)
       assert_equal "Climate Audit", doc.title.strip
     end
     
     it "should have correct WUWT title" do
-      @driver.get(
+      @@driver.get(
         "http://wattsupwiththat.com/2014/07/08/record-levels-of-solar-ultraviolet-measured-in-south-america/"
       )
-      doc = Nokogiri::HTML(@driver.page_source)
+      doc = Nokogiri::HTML(@@driver.page_source)
       assert_equal(
         "Record levels of solar ultraviolet measured in South America | Watts Up With That?", 
         doc.title.strip
