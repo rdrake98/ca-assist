5701b74e427a8a308352769fb56e2a566821b3f6
Richard Drake <rdrake98@gmail.com>
2014-07-09 22:35:15 +0100

    class variables not the right way

--- test.rb
+++ test.rb
@@ -4,61 +4,55 @@ require 'nokogiri'
 
 class TestClimfit < MiniTest::Test
   
+  @@fetches = 0
+  
   @@driver = Selenium::WebDriver.for :firefox, profile: "caa"
 
   Minitest.after_run { @@driver.quit }
   
   def self.get_noko(url)
     puts
-    puts url
+    puts "fetch #{@@fetches += 1}: #{url}"
     @@driver.get url
     Nokogiri::HTML @@driver.page_source
   end
 
   describe "CA" do
     it "should have correct title" do
-      doc = TestClimfit.get_noko "http://climateaudit.org"
+      doc = TestClimfit.get_noko "http://climateaudit.org/"
       assert_equal "Climate Audit", doc.title.strip
     end
   end
     
   describe "WUWT" do
     
-    before do
-      @doc ||= TestClimfit.get_noko(
-        "http://wattsupwiththat.com/2014/07/08/record-levels-of-solar-ultraviolet-measured-in-south-america/"
-      )
-    end
-
-    after do
-      @doc = nil
-    end
+    @@doc = TestClimfit.get_noko(
+      "http://wattsupwiththat.com/2014/07/08/record-levels-of-solar-ultraviolet-measured-in-south-america/"
+    )
 
     it "should have correct title" do 
       assert_equal(
         "Record levels of solar ultraviolet measured in South America | Watts Up With That?", 
-        @doc.title.strip
+        @@doc.title.strip
       )
     end
 
-    it "should have comments" do 
-      comments = @doc.css '.highlander-comment'
+    it "should have some comments" do 
+      comments = @@doc.css '.highlander-comment'
       assert_equal nil, comments.size
     end
   end
 
   describe "Local" do
     
-    before do
-      @doc = TestClimfit.get_noko "file:///Users/richarddrake/web/climfit/CE4.html"
-    end
+    @@doc = TestClimfit.get_noko "file:///Users/richarddrake/web/climfit/CE4.html"
 
     it "should have title" do 
-      assert_equal nil, @doc.title.strip
+      assert_equal "Selection bias in climate model simulations | Climate Etc.", @@doc.title.strip
     end
 
     it "should have comments" do 
-      comments = @doc.css '.highlander-comment'
+      comments = @@doc.css '.highlander-comment'
       assert_equal nil, comments.size
     end
   end
