ac36437d1593b83b52c20ae20bd54686ada23b7e
Richard Drake <rdrake98@gmail.com>
2014-07-12 20:36:45 +0100

    refactor

--- ca-assist.user.js
+++ ca-assist.user.js
@@ -76,27 +76,17 @@ String.prototype.untag = function() {
 
 function empty(data)
 {
-  if(typeof(data) == 'number' || typeof(data) == 'boolean')
-  {
-    return false;
-  }
-  if(typeof(data) == 'undefined' || data === null)
-  {
-    return true;
-  }
-  if(typeof(data.length) != 'undefined')
-  {
-    return data.length == 0;
-  }
-  var count = 0;
-  for(var i in data)
-  {
+  if (typeof(data) == 'number' || typeof(data) == 'boolean')
+    return false
+  if (typeof(data) == 'undefined' || data === null)
+    return true
+  if (typeof(data.length) != 'undefined')
+    return data.length == 0
+  var count = 0
+  for (var i in data)
     if(data.hasOwnProperty(i))
-    {
-      count ++;
-    }
-  }
-  return count == 0;
+      count++
+  return count == 0
 }
 
 /* **************************************************
