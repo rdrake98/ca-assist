6f17b09c1a677f7bb6511b48fbef249eacf87f8e
Richard Drake <rdrake98@gmail.com>
2014-07-12 21:06:15 +0100

    reduced empty

--- ca-assist.user.js
+++ ca-assist.user.js
@@ -72,21 +72,12 @@ String.prototype.untag = function() {
   return this.replace(/<[^>]+>/g, '');
 }
 
-// from James Edwards, http://www.sitepoint.com/testing-for-empty-values/
-
-function empty(data)
-{
-  if (typeof(data) == 'number' || typeof(data) == 'boolean')
-    return false
-  if (typeof(data) == 'undefined' || data === null)
-    return true
-  if (typeof(data.length) != 'undefined')
-    return data.length == 0
-  var count = 0
-  for (var i in data)
-    if(data.hasOwnProperty(i))
-      count++
-  return count == 0
+function empty(data) {
+  if (typeof(data) == 'number' || typeof(data) == 'boolean') return false
+  if (typeof(data) == 'undefined' || data === null) return true
+  if (typeof(data.length) != 'undefined') return data.length == 0
+  for (var i in data) if (data.hasOwnProperty(i)) return false
+  return true
 }
 
 /* **************************************************
