bd6c2ad6852d03df2f33affc313f5bb98c78e366
Richard Drake <rdrake98@gmail.com>
2014-08-04 21:37:09 +0100

    latest jQuery, add missing file

---
+++ license.js
@@ -0,0 +1,28 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* @package Climate Audit Assistant
+* @Copyright ClimateAudit.org 2009+. All right reserved
+* @authors: MrPete and friends
+* @created: January 8, 2010
+* @version 0.1
+* @credits: Inspired by Mafia Wars Autoplayer script and more
+*
+* History:
+* 0.1 Use jQuery 2.0.3 and support Climate Etc.
+* 0.0.9 Support new Lucia version.
+* 0.0.8 All Open Science Blog Ring sites now should work. Comment reorder now optional. Many bugfixes.
+*/
